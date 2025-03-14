83427ffd5d507f9247dbea36fafdb310692fc5ce
ElTheVain <gloryvain39@gmail.com>
2025-03-14 21:28:44 +0700

    Update ganjil_atas_bawah.py

--- ganjil_atas_bawah.py
+++ ganjil_atas_bawah.py
@@ -1,12 +1,12 @@
 def ganjil(bawah, atas):
-    langkah = -1 if bawah > atas else 1
     if bawah % 2 == 0:
-        bawah += langkah
+        bawah += -1 if bawah > atas else 1
+    langkah = -2 if bawah > atas else 2
     hasil = []
-    for i in range(bawah, atas + langkah, 2 * langkah):
-        hasil.append(str(i))
+    while (bawah >= atas if langkah < 0 else bawah <= atas):
+        hasil.append(str(bawah))
+        bawah += langkah
     print("Hasil deret ganjil:", ", ".join(hasil))
-
 bawah = int(input("Masukkan batas bawah: "))
 atas = int(input("Masukkan batas atas: "))
 ganjil(bawah, atas)
